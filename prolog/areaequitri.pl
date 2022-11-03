82d67257d570f9df667192ecfe4aa63d5ef3d0aa
Meetali <102135323+meetalik8@users.noreply.github.com>
2022-11-03 15:31:24 +0530

    Create areaequitri.pl

---
+++ prolog/areaequitri.pl
@@ -0,0 +1,6 @@
+area:-
+   write('Enter the length of side of equilateral triangle:'),
+    read(Lside),
+    write('The area of the triangle is :'),
+    A is (sqrt(3)*Lside*Lside/4),
+    write(A).
