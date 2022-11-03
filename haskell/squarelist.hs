8079058e0a169427994fdcc483167aded3801473
Meetali <102135323+meetalik8@users.noreply.github.com>
2022-11-03 18:39:21 +0530

    Create squarelist.hs

---
+++ haskell/squarelist.hs
@@ -0,0 +1,14 @@
+square[]=[]
+square (x:xs)  =[x*x] ++square(xs)
+main = do
+      putStrLn"Enter 1st element of the list:"
+      ain<-getLine
+      let a=(read ain :: Int)
+      putStrLn"Enter 2nd element of the list:"
+      bin<-getLine
+      let b=(read bin :: Int)
+      putStrLn"Enter 3rd element of the list:"
+      cin<-getLine
+      let c=(read cin :: Int)
+      let list1=[a,b,c]
+      print(square list1)
