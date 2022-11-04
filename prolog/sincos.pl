bde51baae7c5fe7a505e200e0af6302859d4f78f
Meetali <102135323+meetalik8@users.noreply.github.com>
2022-11-04 06:00:35 +0530

    Uses inbuilt function

---
+++ prolog/sincos.pl
@@ -0,0 +1,9 @@
+calc:-
+    write("Enter the angle in radians"),nl,
+    read(X),
+    Y is cos(X),
+    Z is sin(X),
+    write("The cos of the angle is:"),nl,
+    write(Y),nl,
+    write("The sin of the angle is:"),nl,
+    write(Z).
